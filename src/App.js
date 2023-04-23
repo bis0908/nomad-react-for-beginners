f04d8daa640c8fce5e03f8ed7b3add9451b5996b
wskong <63302940+bis0908@users.noreply.github.com>
2023-04-23 11:10:13 +0900

    6.2 Coin tracker

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setcoins] = useState([]);
+  const [coins, setCoins] = useState([]);
   const [money, setMoney] = useState(0);
   const [selectedCoinUSD, setSelectedCoinUSD] = useState(0);
+
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((res) => res.json())
       .then((json) => {
-        setcoins(json);
+        setCoins(json);
         setLoading(false);
       });
   }, []);
@@ -19,52 +20,51 @@ function App() {
   };
 
   const onSelected = (e) => {
-    // console.log("e: ", e);
-    const selectedPrice = e.target.selectedOptions[0].getAttribute("price");
-    setSelectedCoinUSD(parseFloat(selectedPrice));
+    const selectedPrice = parseFloat(e.target.selectedOptions[0].getAttribute("price"));
+    setSelectedCoinUSD(selectedPrice);
   };
 
-  function HowMuchisIt() {
-    return (
-      <input type="number" value={money > 0 ? `${money / selectedCoinUSD}` : 0} disabled></input>
-    );
-  }
+  // function CoinList({ coins, onSelected }) {
+  //   return (
+  //     <select onChange={onSelected}>
+  //       {coins.map((coin) => (
+  //         <option key={coin.id} price={coin.quotes.USD.price.toFixed(8)}>
+  //           {coin.name} ({coin.symbol}): ${coin.quotes.USD.price.toFixed(8)}
+  //         </option>
+  //       ))}
+  //     </select>
+  //   );
+  // }
 
-  function PrintCoins() {
-    return (
-      <select onChange={onSelected}>
-        {coins.map((coin) => {
-          return (
-            <option key={coin.id} price={coin.quotes.USD.price}>
-              {coin.name}({coin.symbol}): {coin.quotes.USD.price} USD
-            </option>
-          );
-        })}
-      </select>
-    );
-  }
-
-  /* 
-    [] option에서 선택한 코인의 USD를 저장한다.
-    [] 이 코인의 USD를 money로 나눈다.
-    [] 나눈 USD를 disabled된 input에 입력한다.
-    [] 단, money 값이 바뀔때만 업데이트 되어야 함.
-  */
   return (
     <div>
-      {/* <h1>The Coins! {loading ? "" : `${coins.length}`}</h1> */}
-      <h1>The Coins! {loading ? "" : `${coins.length}`}</h1>
+      <h1>The Coins! {loading ? "" : coins.length}</h1>
       <div>
-        if i have this dollars...
-        <input type="number" value={money} onChange={onChange}></input>
+        If I have this dollars...
+        <input type="number" value={money} onChange={onChange} min="0" />
       </div>
       <div>
-        i can buy coin qty
-        {/* <input type="number" value={money} disabled></input> */}
-        <HowMuchisIt />
+        I can get coin this much
+        <input
+          type="number"
+          value={money > 0 && selectedCoinUSD !== "" ? money / selectedCoinUSD : 0}
+          disabled
+        />
       </div>
-      {loading ? <strong>Loading...</strong> : <PrintCoins />}
+      {loading ? <strong>Loading...</strong> : <CoinList coins={coins} onSelected={onSelected} />}
     </div>
+  );
+}
+
+function CoinList({ coins, onSelected }) {
+  return (
+    <select onChange={onSelected}>
+      {coins.map((coin) => (
+        <option key={coin.id} price={coin.quotes.USD.price.toFixed(8)}>
+          {coin.name} ({coin.symbol}): ${coin.quotes.USD.price.toFixed(8)}
+        </option>
+      ))}
+    </select>
   );
 }
 
